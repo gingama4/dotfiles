8c33af74bb0d74a4851be8f036168ad64098f82b
gingama4 <gingama4@gmail.com>
2020-09-05 06:56:48 +0900

    update git setup

--- git/install.sh
+++ git/install.sh
@@ -2,14 +2,25 @@
 ESC=$(printf '\033') RESET="${ESC}[0m"
 
 printf "${ESC}[32m%s${RESET}\n" "git setup"
-printf "${ESC}[36m%s${RESET}\n" "Please enter your name and email address."
-printf "${ESC}[36m%s${RESET}\n" "(These are used by gitconfig)"
+if [ ! -f $PWD/config.txt ];then
+    printf "${ESC}[36m%s${RESET}\n" "Please enter your name and email address."
+    printf "${ESC}[36m%s${RESET}\n" "(These are used by gitconfig)"
 
-echo -n INPUT_USER_NAME:
-read username
+    echo -n INPUT_USER_NAME:
+    read username
 
-echo -n INPUT_EMAIL_ADD:
-read useremail
+    echo -n INPUT_EMAIL_ADD:
+    read useremail
+
+    echo "USER=${username}" > config.txt
+    echo "MAIL=${useremail}" >> config.txt
+else
+    . $PWD/config.txt
+    echo $USER
+    echo $MAIL
+    username=$USER
+    useremail=$MAIL
+fi
 
 cp $PWD/gitconfig ~/.gitconfig
 git config --global user.name $username
