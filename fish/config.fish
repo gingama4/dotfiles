a938fd11952450f5284a839444817fe54ef4f5d8
gingama4 <gingama4@gmail.com>
2020-10-06 09:47:48 +0900

    update 256color

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,8 @@ set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
 # anyenv
 status --is-interactive; and source (anyenv init -|psub)
 
+set -g TERM xterm-256color
+
 ##= Alias ====================================
 alias lg='lazygit'
 
