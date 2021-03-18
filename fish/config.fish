b143bea2eb29b508cae24d88b06980d1582f2aaa
gingama4 <gingama4@gmail.com>
2021-03-18 15:23:49 +0900

    powerline-go

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,8 @@ alias lg='lazygit'
 alias ld='lazydocker'
 alias dc='docker-compose'
 
+function fish_prompt
+    set module "user,cwd,git,root,newline"
+    set duration (math -s6 "$CMD_DURATION / 1000")
+    powerline-go -modules $module -error $status -shell bare
+end
