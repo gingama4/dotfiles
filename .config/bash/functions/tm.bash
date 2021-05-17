6b0118868e414aacf1753bedb298b4475934eb02
gingama4 <gingama4@gmail.com>
2021-05-18 01:27:12 +0900

    change tm bahavior
    from fzf-sample

--- .config/bash/functions/tm.bash
+++ .config/bash/functions/tm.bash
@@ -1,30 +1,11 @@
 [ -n "$DOT_DEBUG" ] && echo "Load tm.bash"
 
 tm() {
-  if [[ -n "$TMUX" ]]; then
-    local tmode="switch-client"
-  else
-    local tmode="attach-session"
+  [[ -n "$TMUX" ]] && tmode="switch-client" || tmode="attach-session"
+
+  if [ $1 ]; then
+    tmux $tmode -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
   fi
 
-  if [ $# != 0 ]; then
-    if tmux $tmode -t $1 2>/dev/null; then
-      tmux $tmode -t $1
-    else
-      tmux new-session -d -s $1 && tmux $tmode -t $1
-    fi
-    return 0
-  fi
-
-  local ID=$(tmux list-sessions 2> /dev/null)
-  if [ -z "$ID" ]; then
-    tmux new-session
-    return 0
-  fi
-
-  local session=$(tmux list-sessions -F "#{session_name}" | fzf --select-1)
-
-  if [ -n "$session" ]; then
-    tmux $tmode -t "$session"
-  fi
+  session=$(tmux list-session -F "#{session_name}" 2>/dev/null | fzf --exit-0) && tmux $change -t "$session" || echo "No sessions found."
 }
