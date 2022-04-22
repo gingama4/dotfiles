5f370f9acf081e0dc070a91222463b2c5958b485
gingama4 <gingama4@gmail.com>
2022-04-23 02:41:22 +0900

    update polybar & add rofi,tilix

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,12 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar main &
+  done
+else
+  polybar main &
+fi
 
 echo "Polybar launched..."
