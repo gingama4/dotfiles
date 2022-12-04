efaf769b87b20edd637139430144ff590dd14790
gingama4 <gingama4@gmail.com>
2022-12-05 01:55:00 +0900

    code用設定を分割

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,6 @@
-if vim.g.vscode then
-  require('vscode.init')
-else
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
-  require('rc.base')
-  require('plugins')
-end
+require('rc.base')
+require('plugins')
 
---
+++ config/nvim/vscode.vim
@@ -0,0 +1,5 @@
+" VSCode
+
+nnoremap fp <CMD>call VSCodeNotify('workbench.action.quickOpen')<CR>
+
+set clipboard=unnamed,unnamedplus
