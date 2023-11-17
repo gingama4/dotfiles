9c7153078a24005aeefbe31a8aacb4d917f711eb
gingama4 <gingama4@ginArch.localdomain>
2023-11-18 01:15:02 +0900

    Fix install dotfiles

--- install.sh
+++ install.sh
@@ -3,13 +3,16 @@
 set -ue
 
 INSTALL_DIR="${INSTALL_DIR:-$HOME/ghq/github.com/gingama4/dotfiles}"
+CI=${CI:-0}
 
-#if [ -d "$INSTALL_DIR" ]; then
-#    echo "Updating dotfiles..."
-#    git -C "$INSTALL_DIR" pull
-#else
-#    echo "Installing dotfiles..."
-#    git clone https://github.com/gingama4/dotfiles "$INSTALL_DIR"
-#fi
+if [ $CI -ne 1 ]; then
+    if [ -d "$INSTALL_DIR" ]; then
+        echo "Updating dotfiles..."
+        git -C "$INSTALL_DIR" pull
+    else
+        echo "Installing dotfiles..."
+        git clone https://github.com/gingama4/dotfiles "$INSTALL_DIR"
+    fi
+fi
 
 /usr/bin/env bash "$INSTALL_DIR/scripts/setup.sh" "$@"
