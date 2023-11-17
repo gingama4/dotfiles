0b1fe30d24ba050c8c690d4cca784c1c03325888
gingama4 <gingama4@ginArch.localdomain>
2023-11-18 00:32:15 +0900

    Add common install script

--- install.sh
+++ install.sh
@@ -2,3 +2,14 @@
 
 set -ue
 
+INSTALL_DIR="${INSTALL_DIR:-$HOME/ghq/github.com/gingama4/dotfiles}"
+
+#if [ -d "$INSTALL_DIR" ]; then
+#    echo "Updating dotfiles..."
+#    git -C "$INSTALL_DIR" pull
+#else
+#    echo "Installing dotfiles..."
+#    git clone https://github.com/gingama4/dotfiles "$INSTALL_DIR"
+#fi
+
+/usr/bin/env bash "$INSTALL_DIR/scripts/setup.sh" "$@"
---
+++ scripts/setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -ue
+
+REPO_DIR="$(cd "$(dirname "$0")/.."; pwd)"
+CONFIGS_DIR="${REPO_DIR}/.config"
+
+. "${CONFIGS_DIR}/zsh/zshenv.zsh"
+
+. "${REPO_DIR}/scripts/common.sh"
+
+setup_files="${REPO_DIR}/scripts/**/setup.sh"
+for filepath in $setup_files; do
+    . "$filepath"
+done
+
+setup_linux() {
+    local distro
+    distro=$(awk '{print $1; exit}' /etc/issue)
+    title "Setting up ${distro}"
+
+    setup_zsh
+}
+
+execute_setup() {
+    setup_linux
+
+    echo ""
+    success "Setup is complete."
+}
+
+execute_setup
