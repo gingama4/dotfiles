c18cfc11a156e3caa6d6f202340552e8c5b99c38
gingama4 <gingama4@gmail.com>
2023-11-30 02:00:19 +0900

    Add gitsigns

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -40,5 +40,15 @@ return {
     },
     config = require 'plugins.configs.toggleterm'
   },
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 500,
+      },
+    },
+  },
 }
 
