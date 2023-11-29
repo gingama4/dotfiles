c14e492ad56453ca6569c862a4d24b56958a68eb
gingama4 <gingama4@gmail.com>
2023-11-30 02:17:20 +0900

    Add Tab and S-Tab mapping to lsp-cmp

--- .config/nvim/lua/plugins/configs/nvim-cmp.lua
+++ .config/nvim/lua/plugins/configs/nvim-cmp.lua
@@ -8,6 +8,11 @@ return function()
     winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
   }
 
+  local function has_words_before()
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+  end
+
   cmp.setup({
     enabled = function()
       return true
@@ -38,6 +43,22 @@ return function()
       ['<C-k>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
       ['<C-j>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
       ['<C-y>'] = cmp.mapping.disable,
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
     },
     sources = cmp.config.sources {
       { name = 'nvim_lsp', priority = 1000 },
