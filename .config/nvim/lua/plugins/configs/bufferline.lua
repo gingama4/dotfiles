8245094df26f3e390ca7ba1fa57d98af78e5a057
gingama4 <gingama4@gmail.com>
2023-11-30 01:51:14 +0900

    Add keymap

--- .config/nvim/lua/plugins/configs/bufferline.lua
+++ .config/nvim/lua/plugins/configs/bufferline.lua
@@ -8,10 +8,12 @@ return function(_, opts)
 
   vim.keymap.set('n', 'L', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
   vim.keymap.set('n', 'H', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
-  vim.keymap.set('n', '<C-l>', ':BufferLineMoveNext<CR>', { noremap = true, silent = false })
-  vim.keymap.set('n', '<C-h>', ':BufferLineMovePrev<CR>', { noremap = true, silent = false })
+  vim.keymap.set('n', ']b', ':BufferLineMoveNext<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '[b', ':BufferLineMovePrev<CR>', { noremap = true, silent = true })
   vim.keymap.set('n', '<leader>b', ':BufferLinePick<CR>', { noremap = true, silent = true })
 
-  vim.keymap.set('n', '<leader>D', ':BufferLineCloseRight<CR>', { noremap = true, silent = false })
-  vim.keymap.set('n', '<leader>d', ':BufferLinePickClose<CR>', { noremap = true, silent = false })
+  vim.keymap.set('n', '<leader>wl', ':BufferLineCloseRight<CR>', { noremap = true, silent = false })
+  vim.keymap.set('n', '<leader>wh', ':BufferLineCloseLeft<CR>', { noremap = true, silent = false })
+  vim.keymap.set('n', '<leader>wb', ':BufferLineCloseOthers<CR>', { noremap = true, silent = false })
+  vim.keymap.set('n', '<leader>we', ':BufferLinePickClose<CR>', { noremap = true, silent = false })
 end
