bb4fde2ee91ef9ce5de0c2ae07c1a84efe8d7be0
gingama4 <gingama4@gmail.com>
2024-02-15 02:13:51 +0900

    nvim: onefetch dashboard for alpha

--- .config/nvim/lua/plugins/configs/alpha.lua
+++ .config/nvim/lua/plugins/configs/alpha.lua
@@ -1,3 +1,36 @@
+local function capture(cmd, raw)
+  local f = assert(io.popen(cmd, "r"))
+  local s = assert(f:read("*a"))
+  f:close()
+  if raw then
+    return s
+  end
+  s = string.gsub(s, "^%s+", "")
+  s = string.gsub(s, "%s+$", "")
+  s = string.gsub(s, "[\n\r]+", " ")
+  return s
+end
+
+local function split(source, sep)
+  local result, i = {}, 1
+  while true do
+    local a, b = source:find(sep)
+    if not a then
+      break
+    end
+    local candidat = source:sub(1, a - 1)
+    if candidat ~= "" then
+      result[i] = candidat
+    end
+    i = i + 1
+    source = source:sub(b + 1)
+  end
+  if source ~= "" then
+    result[i] = source
+  end
+  return result
+end
+
 return function(_, opts)
   require('alpha').setup(opts.config)
   local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
@@ -16,13 +49,27 @@ return function(_, opts)
     end,
   })
   -- Disable the statusline, tabline and cmdline while the alpha dashboard is open
+  vim.api.nvim_create_augroup("vimrc_alpha", { clear = true })
   autocmd('User', {
+    group = "vimrc_alpha",
     pattern = 'AlphaReady',
-    desc = 'disable status, tabline and cmdline for alpha',
     callback = function()
       vim.go.laststatus = 0
       vim.opt.showtabline = 0
       vim.opt.cmdheight = 0
+      if vim.fn.executable("onefetch") == 1 then
+        local header = split(
+          capture(
+            [[onefetch 2>/dev/null | sed 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g']],
+            true
+          ),
+          "\n"
+        )
+        if next(header) ~= nil then
+          require("alpha.themes.dashboard").section.header.val = header
+          require("alpha").redraw()
+        end
+      end
     end,
   })
   autocmd('BufUnload', {
