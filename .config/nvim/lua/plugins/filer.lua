31e9510353b4408cf2394ea3fec21490ed2df0ae
gingama4 <gingama4@gmail.com>
2024-02-15 02:24:51 +0900

    nvim: move neo-tree config to plugin opts

--- .config/nvim/lua/plugins/filer.lua
+++ .config/nvim/lua/plugins/filer.lua
@@ -18,19 +18,28 @@ return {
   },
   {
     'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
+    branch = 'main',
     keys = {
       { "<leader>e", ":Neotree toggle<CR>", desc = "toggle neo-tree", silent = true },
     },
+    cmd = "Neotree",
     dependencies = {
       { 'nvim-lua/plenary.nvim' },
       { 'nvim-tree/nvim-web-devicons' },
       { 'MunifTanjim/nui.nvim' },
     },
+    init = function() vim.g.neo_tree_remove_legacy_commands = true end,
     opts = function ()
-      return {}
+      return {
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+          }
+        },
+      }
     end,
-    config = require'plugins.configs.neo-tree'
   },
 }
 
