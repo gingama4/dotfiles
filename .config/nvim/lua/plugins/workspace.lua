b9e06a64a67fa0dbfbd07bd1191be1e7ab781a75
gingama4 <gingama4@gmail.com>
2024-02-15 01:35:25 +0900

    nvim: remove workspace config

--- .config/nvim/lua/plugins/workspace.lua
+++ .config/nvim/lua/plugins/workspace.lua
@@ -2,15 +2,6 @@ return {
   'natecraddock/workspaces.nvim',
   lazy = false,
   opts = {},
-  config = function()
-    require('workspaces').setup({
-      hooks = {
-        open = function ()
-          local workspace = require 'utils.workspace'
-          workspace.load()
-        end,
-      },
-    })
-  end,
+  config = function() require('workspaces').setup() end,
 }
 
--- .config/nvim/lua/workspaces/example/rc/rc.lua
+++
@@ -1,8 +0,0 @@
-local options = {
-  number = true,
-}
-
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
-
