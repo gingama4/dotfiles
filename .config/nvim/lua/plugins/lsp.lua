c567f036164bfe60795d0e7de00f4c61f36bc5df
gingama4 <gingama4@gmail.com>
2024-02-20 02:19:59 +0900

    nvim: add completion plugin

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -22,6 +22,7 @@ return {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
     },
     config = require'plugins.configs.nvim-cmp',
   },
