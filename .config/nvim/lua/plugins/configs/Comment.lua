425271a6d9af9a2fde1b1958b0ab96d42c85f494
gingama4 <gingama4@gmail.com>
2024-02-22 03:35:37 +0900

    nvim: add Comment plugin

---
+++ .config/nvim/lua/plugins/configs/Comment.lua
@@ -0,0 +1,22 @@
+require('Comment').setup({
+  padding = true,
+  ignore = nil,
+  mappings = {
+    basic = true,
+    extra = false,
+  },
+  toggler = {
+    line = 'gcc',
+    block = 'gcc',
+  },
+  opleader = {
+    line = 'gc',
+    block = 'gb',
+  },
+  pre_hook = nil,
+  post_hook = nil,
+})
+
+vim.api.nvim_set_keymap('n', '<C-/>', "<Cmd>lua require('Comment.api').toggle.linewise.current()<CR>", {})
+vim.api.nvim_set_keymap('i', '<C-/>', "<Esc>:<C-u>lua require('Comment.api').toggle.linewise.current()<CR>\"_cc", {})
+vim.api.nvim_set_keymap('v', '<C-/>', "gc", {})
---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    'numToStr/Comment.nvim',
+    event = 'VeryLazy',
+    config = function() require 'plugins.configs.Comment' end
+  },
+}
