129a3ef60298d7040d85001e4242bc1ea4c6d278
gingama4 <gingama4@gmail.com>
2024-02-22 03:58:31 +0900

    nvim: fix fidget setup

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -39,14 +39,15 @@ return {
   {
     'j-hui/fidget.nvim',
     event = 'VimEnter',
+    config = function() require('fidget').setup() end,
   },
   {
     'akinsho/toggleterm.nvim',
     event = { "BufReadPost", "BufNewFile" },
     keys = {
-      { '<leader>t', ':ToggleTerm direction=float<CR>' },
+      { '<leader>t',  ':ToggleTerm direction=float<CR>' },
       { '<C-Space>t', ':ToggleTerm direction=horizontal<CR>' },
-      { '<leader>g', '<cmd>lua __lazygit_toggle()<CR>' },
+      { '<leader>g',  '<cmd>lua __lazygit_toggle()<CR>' },
     },
     opts = {
       autochdir = true,
@@ -64,4 +65,3 @@ return {
     },
   },
 }
-
