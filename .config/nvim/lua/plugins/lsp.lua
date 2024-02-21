577c6b60d182b59ab55b144e62f3a4ff07cc964a
gingama4 <gingama4@gmail.com>
2024-02-22 03:19:41 +0900

    nvim: overhaul nvim-cmp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -23,6 +23,11 @@ return {
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lua',
+      {
+        'zbirenbaum/copilot-cmp',
+        config = true,
+      },
+      'saadparwaiz1/cmp_luasnip',
     },
     config = require 'plugins.configs.nvim-cmp',
   },
