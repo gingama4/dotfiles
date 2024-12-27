2d6ac64696ed0ee3f78c1dee245bef1e242dfc82
gingama4 <gingama4@gmail.com>
2024-12-28 01:00:19 +0900

    nvim: add base plugins

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,12 @@
+return {
+  { "folke/lazy.nvim", version = "*" },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {},
+    config = function(_, opts)
+      require("snacks").setup(opts)
+    end,
+  }
+}
