5dc334b1bff67d033ef6ecd370c38a9787f5fe62
gingama4 <gingama4@gmail.com>
2024-12-29 01:02:02 +0900

    nvim: remove fidget
    instead use noice

--- .config/nvim/lua/plugins/ui/other.lua
+++ .config/nvim/lua/plugins/ui/other.lua
@@ -1,10 +1,5 @@
 return {
   {
-    'j-hui/fidget.nvim',
-    event = 'VimEnter',
-    config = function() require('fidget').setup() end,
-  },
-  {
     'akinsho/toggleterm.nvim',
     event = { "BufReadPost", "BufNewFile" },
     keys = {
