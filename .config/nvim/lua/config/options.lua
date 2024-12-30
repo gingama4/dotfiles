ea980acfa5e336c156fddcea8faed0d09a858674
gingama4 <gingama4@gmail.com>
2024-12-30 16:04:39 +0900

    nvim: config LazyVim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.mapleader = " "
+
+vim.opt.backup = true
+vim.opt.cmdheight = 0
+vim.opt.backupdir = vim.fn.stdpath("state") .. "/backup"
+vim.opt.mousescroll = "ver:1,hor:4"
