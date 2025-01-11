4747ecdf2117695579cb16abb63c8e1578585730
gingama4 <gingama4@gmail.com>
2025-01-12 00:50:30 +0900

    nvim: change NeoTree key-config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,6 +1,10 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+      { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+    },
     opts = {
       filesystem = {
         filtered_items = {
