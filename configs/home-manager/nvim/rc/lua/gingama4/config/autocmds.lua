793abedad7e14df41d48db330de6d19469b83542
gingama4 <gingama4@gmail.com>
2025-02-20 00:04:57 +0900

    nvim: config auto_reload

--- configs/home-manager/nvim/rc/lua/gingama4/config/autocmds.lua
+++ configs/home-manager/nvim/rc/lua/gingama4/config/autocmds.lua
@@ -1,3 +1,9 @@
 local function augroup(name)
-  return vim.api.nvim_create_augroup("ginvim_" .. name, { alear = true })
+  return vim.api.nvim_create_augroup("ginvim_" .. name, { clear = true })
 end
+
+vim.api.nvim_create_autocmd({ "WinEnter", "FocusGained", "BufEnter" }, {
+  group = augroup("auto_reload"),
+  pattern = "*",
+  command = "checktime",
+})
