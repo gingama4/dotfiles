27da424666b4d143fd8418a7eb2d26498c8b9679
gingama4 <gingama4@gmail.com>
2025-02-23 03:16:01 +0900

    nvim: config Snacks.terminal

--- configs/home-manager/nvim/rc/lua/gingama4/config/keymaps.lua
+++ configs/home-manager/nvim/rc/lua/gingama4/config/keymaps.lua
@@ -1,11 +1,11 @@
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+local map = vim.keymap.set
 
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
--- [_Lsp]
-vim.keymap.set("n", ";", "<Nop>", { noremap = true, silent = true })
-vim.keymap.set("n", "[_Lsp]", "<Nop>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", ";", "[_Lsp]", {})
+-- Terminal
+map("n", "<c-/>", function() Snacks.terminal() end, { desc = "Terminal" })
+map("t", "<c-/>", "<cmd>close<cr>", { desc = "Hide terminal" } )
+
--- configs/home-manager/nvim/rc/lua/gingama4/plugins/snacks.lua
+++ configs/home-manager/nvim/rc/lua/gingama4/plugins/snacks.lua
@@ -1,3 +1,12 @@
+-- Term Key-Mapping
+local function term_nav(dir)
+  return function(self)
+    return self:is_floating() and "<c-" .. dir .. ">" or vim.schedule(function()
+      vim.cmd.wincmd(dir)
+    end)
+  end
+end
+
 return {
   name = "folke/snacks.nvim",
   dir = "@snacks_nvim@",
@@ -20,6 +29,17 @@ return {
     lazygit = { enabled = true },
     notifier = { enabled = true },
     picker = { enabled = true },
+    terminal = {
+      enabled = true,
+      win = {
+        keys = {
+          nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+          nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+          nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+          nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+        },
+      },
+    },
   },
   keys = {
     -- Explorer
