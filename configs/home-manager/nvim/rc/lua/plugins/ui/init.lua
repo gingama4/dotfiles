811c7cf654a8726066e51d2318d9d0364236362f
gingama4 <gingama4@gmail.com>
2025-03-11 23:29:32 +0900

    nvim: modify float window transparent

--- configs/home-manager/nvim/rc/lua/plugins/ui/init.lua
+++ configs/home-manager/nvim/rc/lua/plugins/ui/init.lua
@@ -10,6 +10,15 @@ return {
     opts = {
       compile = false,
       transparent = true,
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        }
+      end,
     },
     ---@param opts KanagawaConfig
     config = function(_, opts)
