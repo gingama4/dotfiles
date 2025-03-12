d0de3669ece0cfbe68ee2a5ed10bb064ba9fa63d
gingama4 <gingama4@gmail.com>
2025-03-13 00:08:35 +0900

    nvim: modify lsp keyconfig

--- configs/home-manager/nvim/rc/lua/config/keymaps.lua
+++ configs/home-manager/nvim/rc/lua/config/keymaps.lua
@@ -5,7 +5,13 @@ map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
--- Terminal
-map("n", "<c-/>", function() Snacks.terminal() end, { desc = "Terminal" })
-map("t", "<c-/>", "<cmd>close<cr>", { desc = "Hide terminal" } )
+-- Lsp
+map("n", "K", function()
+  vim.lsp.buf.hover({ border = "rounded", title = "LSP Hover" })
+end, { desc = "LSP Hover" })
 
+-- Terminal
+map("n", "<c-/>", function()
+  Snacks.terminal()
+end, { desc = "Terminal" })
+map("t", "<c-/>", "<cmd>close<cr>", { desc = "Hide terminal" })
