d06c53522a4dea691c21cc1a73e9bc2531ce3eca
gingama4 <gingama4@gmail.com>
2025-10-08 01:25:25 +0900

    feat(nvim): which-keyの設定を変更

--- configs/nvim/lua/plugins/editor.lua
+++ configs/nvim/lua/plugins/editor.lua
@@ -3,5 +3,8 @@ local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 -- Which Key
 later(function()
   add('folke/which-key.nvim')
-  require('which-key').setup()
+  require('which-key').setup({
+    preset = "helix",
+    default = {},
+  })
 end)
