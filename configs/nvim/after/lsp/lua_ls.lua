a2d6493ad14f40330abe08769253b828bbcd7f3e
gingama4 <gingama4@gmail.com>
2025-12-12 00:32:24 +0900

    feat(nvim): lua_lsの設定を更新

--- configs/nvim/after/lsp/lua_ls.lua
+++ configs/nvim/after/lsp/lua_ls.lua
@@ -1,30 +1,25 @@
 return {
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if
-        path ~= vim.fn.stdpath("config")
-        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-      then
-        return
-      end
-    end
-    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-      runtime = { version = "LuaJIT" },
+  on_attach = function(client, buf_id)
+    client.server_capabilities.completionProvider.triggerCharacters = { ".", ":", "#", "(" }
+
+    local opts = { buffer = buf_id, desc = "Lua source definition" }
+    vim.keymap.set("n", "<Leader>ls", GinVim.luals_unique_definition, opts)
+  end,
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+      diagnostics = {
+        workspaceDelay = -1,
+      },
       workspace = {
+        ignoreSubmodules = true,
         checkThirdParty = false,
         library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
           "${3rd}/luv/library",
           "${3rd}/busted/library",
         }),
       },
-    })
-  end,
-  settings = {
-    Lua = {
-      diagnostics = {
-        unusedLocalExclude = { "_*" },
-      },
+      telemetry = { enable = false },
     },
   },
 }
