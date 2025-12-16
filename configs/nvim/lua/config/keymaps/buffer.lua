9bcb77492ccb0662176db8c98195ef2d0f65c646
gingama4 <gingama4@gmail.com>
2025-12-17 00:21:03 +0900

    feat(nvim): バッファ関連のキーマップを削除

--- configs/nvim/lua/config/keymaps/buffer.lua
+++ configs/nvim/lua/config/keymaps/buffer.lua
@@ -1,7 +1,3 @@
 return {
-  { "H", "<cmd>bprev<cr>", desc = "Previous" },
-  { "L", "<cmd>bnext<cr>", desc = "Next" },
-  { "<leader>bh", "<cmd>bprev<cr>", desc = "Previous" },
-  { "<leader>bl", "<cmd>bnext<cr>", desc = "Next" },
   { "<leader>bd", "<cmd>bdelete<cr>", desc = "Delete" },
 }
