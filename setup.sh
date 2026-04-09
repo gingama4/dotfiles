10340e2baf75de8b19a0fe645fbd17df96c60c43
gingama4 <me@gingama4.com>
2026-04-10 00:25:02 +0900

    feat(script): セットアップ用スクリプトを仮追加

---
+++ setup.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+if [ "${DOTFILES_DEBUG:-}" ] ; then
+  set -x
+fi
+
+declare -r DOTFILES_REPO_URL="https://github.com/gingama4/dotfiles"
+
+declare -r DOTFILES_DIALOG='
+DOTFILES
+author: gingama4
+repository: https://github.com/gingama4/dotfiles
+'
+
+declare -r OS_TYPE="$(uname -s)"
+
+function initialize_os_env() {
+  if [[ "${OS_TYPE}" == "Darwin" ]]; then
+    echo "Detected macOS environment."
+    initialize_macos
+  elif [[ "${OS_TYPE}" == "Linux" ]]; then
+    echo "Detected Linux environment."
+  else
+    echo "Unsupported OS type: ${OS_TYPE}"
+    exit 1
+  fi
+}
+
+function initialize_macos() {
+  function is_homebrew_installed() {
+    command -v brew &> /dev/null
+  }
+
+  # Install Homebrew if it's not already installed
+  if ! is_homebrew_installed; then
+    echo "Homebrew not found. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  else
+    echo "Homebrew is already installed."
+  fi
+
+  # Setup Homebrew envvars.
+  if [[ $(arch) == "arm64" ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  else
+    echo "Invalid CPU arch: $(arch)" >&2
+    exit 1
+  fi
+}
+
+function initialize_dotfiles() {
+  run_chezmoi
+}
+
+function run_chezmoi() {
+  local bin_dir="${HOME}/.local/bin"
+  export PATH="${PATH}:${bin_dir}"
+
+  # Download chezmoi binary
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "${bin_dir}"
+  local chezmoi_cmd="${bin_dir}/chezmoi"
+
+  command -v "${chezmoi_cmd}" &> /dev/null || {
+    echo "Error: chezmoi command not found after installation." >&2
+    exit 1
+  }
+
+  # Run chezmoi init
+  "${chezmoi_cmd}" init "${DOTFILES_REPO_URL}"
+
+  # Purge the binary og the chezmoi
+  rm -fv "${chezmoi_cmd}"
+}
+
+function main() {
+  echo "${DOTFILES_DIALOG}"
+
+  initialize_os_env
+  initialize_dotfiles
+}
+
+main
