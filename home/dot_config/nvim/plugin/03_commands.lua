6fa5f46d2966d2ea3c7da7d3dc79185201047cb8
gingama4 <me@gingama4.com>
2026-08-04 01:29:45 +0900

    feat(nvim): 不要になったプラグインを削除し、lockファイルと同期するコマンドを追加

--- home/dot_config/nvim/plugin/03_commands.lua
+++ home/dot_config/nvim/plugin/03_commands.lua
@@ -1,6 +1,27 @@
+local function delPack()
+  local unused = vim.iter(vim.pack.get(nil, { info = false }))
+    :filter(function(plugin)
+      return not plugin.active
+    end)
+    :map(function(plugin)
+      return plugin.spec.name
+    end)
+    :totable()
+
+  if #unused > 0 then
+    vim.pack.del(unused)
+  end
+end
+
 vim.api.nvim_create_user_command("GinPackUpdate", function()
+  delPack()
   vim.pack.update()
 end, { desc = "Update Packages" })
+
+vim.api.nvim_create_user_command("GinPackSync", function()
+  delPack()
+  vim.pack.update(nil, { target = 'lockfile' })
+end, { desc = "Sync Packages" })
 
 -- Add diff to commit message
 GinVim.create_autocmd("FileType", "gitcommit", function(args)
